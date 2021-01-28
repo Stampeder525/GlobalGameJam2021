91f20260ebb1916522cd315da9099b64cbef65c0
MMallea <40362079+MMallea@users.noreply.github.com>
2021-01-28 18:39:07 -0500

    Playstation grahpics

---
+++ Assets/CamPostProcess.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CamPostProcess : MonoBehaviour
+{
+    public Material material;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if(material != null)
+            Graphics.Blit(source, destination, material);
+    }
+}
