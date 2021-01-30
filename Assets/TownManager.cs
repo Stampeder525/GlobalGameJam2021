d6fb9f7b52bae9c6bd67dcfcd3e82cb8bc4d4ec2
MMallea <40362079+MMallea@users.noreply.github.com>
2021-01-30 17:26:33 -0500

    Fixed lighting + town manager

--- Assets/TownManager.cs
+++ Assets/TownManager.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class TownManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int puzzleHomeIndex = 0;
+
+    public GameObject defaultLawnObj;
+
+    public GameObject clueGiverLawn1;
+    public GameObject clueGiverLawn2;
+    public GameObject clueGiverLawn3;
+    public GameObject clueGiverLawn4;
+
+    private GameObject[] leftLawnTransformList;
+    private GameObject[] rightLawnTransformList;
+
+    private void Start()
     {
-        
+        //Populate town transform list
+        leftLawnTransformList = GameObject.FindGameObjectsWithTag("LeftLawnTransform");
+        rightLawnTransformList = GameObject.FindGameObjectsWithTag("RightLawnTransform");
+        SpawnDefaultHomes();
+        SpawnSpecialLawns();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnDefaultHomes()
     {
-        
+        if (defaultLawnObj == null)
+            return;
+
+        for(int i = 0; i < leftLawnTransformList.Length; i++)
+        {
+            Transform lawnTransform = leftLawnTransformList[i].transform;
+            Instantiate(defaultLawnObj, lawnTransform);
+        }
+
+        for (int i = 0; i < rightLawnTransformList.Length; i++)
+        {
+            Transform lawnTransform = rightLawnTransformList[i].transform;
+            Instantiate(defaultLawnObj, lawnTransform);
+        }
+    }
+
+    private void SpawnSpecialLawns()
+    {
+        Transform lawnTransform = rightLawnTransformList[10].transform;
+
+        //Delete home there
+        Destroy(lawnTransform.GetChild(0).gameObject);
+
+        //Add prefab
+        if(clueGiverLawn1 != null)
+            Instantiate(clueGiverLawn1, lawnTransform);
+
     }
 }
