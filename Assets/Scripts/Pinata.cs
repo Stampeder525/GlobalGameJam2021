8df60d68593f53dab1ec973e816226d899b32373
Stampeder525 <lorenheubertaubry@gmail.com>
2021-01-30 23:28:23 -0500

    puzzle 4

---
+++ Assets/Scripts/Pinata.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class Pinata : MonoBehaviour
+{
+    public GameObject viscera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log(other.gameObject.tag);
+        if(other.gameObject.tag == "Item") {
+            Debug.Log("Pinata broken!");
+            DialogueLua.SetVariable("PinataBroken", true);
+            viscera.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
+    }
+}
