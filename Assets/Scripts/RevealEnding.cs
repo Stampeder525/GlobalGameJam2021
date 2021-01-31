cb40e0fea5267a971e2a4ac5b78ee61a10f4ff67
Stampeder525 <lorenheubertaubry@gmail.com>
2021-01-31 11:53:01 -0500

    ending

---
+++ Assets/Scripts/RevealEnding.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+
+public class RevealEnding : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool show = true;
+    private int neighborsComplete;
+
+    public GameObject target = null;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        neighborsComplete = DialogueLua.GetVariable("NeighborsComplete").AsInt;
+        if(neighborsComplete >= 5) {
+            if(target == null){
+                gameObject.SetActive(show);
+            }
+            else {
+                target.SetActive(show);
+            }
+        } 
+    }
+}
