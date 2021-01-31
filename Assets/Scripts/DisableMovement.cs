be8d4e5ed65c314dcad8074124c93c77c86cf500
Stampeder525 <lorenheubertaubry@gmail.com>
2021-01-31 12:25:18 -0500

    fixes

--- Assets/Scripts/DisableMovement.cs
+++ Assets/Scripts/DisableMovement.cs
@@ -17,11 +17,14 @@ public class DisableMovement : MonoBehaviour
         player.GetComponent<CustomCharacterController>().enabled = false;
         player.GetComponent<SwapViews>().enabled = false;
         player.GetComponent<ProximitySelector>().enabled = false;
+        player.GetComponent<PickupItem>().enabled = false;
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
 
     public void StartMovement() {
         player.GetComponent<CustomCharacterController>().enabled = true;
         player.GetComponent<SwapViews>().enabled = true;
         player.GetComponent<ProximitySelector>().enabled = true;
+        player.GetComponent<PickupItem>().enabled = true;
     }
 }
