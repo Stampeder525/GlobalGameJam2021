fa4fe89a5949b9756d62c487f97abf3971d502a2
Stampeder525 <lorenheubertaubry@gmail.com>
2021-01-31 14:34:17 -0500

    pause menu

---
+++ Assets/Scripts/EscapeToPause.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeToPause : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject pauseMenu;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            bool isActive = pauseMenu.activeInHierarchy;
+            pauseMenu.SetActive(!isActive);
+        }
+    }
+}
